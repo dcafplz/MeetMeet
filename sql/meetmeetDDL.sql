3675c1ca1730451b54186f536a23357fe779a57c
cho2 <chso1324@naver.com>
2022-07-18 10:48:47 +0900

    Meeting DDL 수정

--- sql/meetmeetDDL.sql
+++ sql/meetmeetDDL.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE 데이터베이스이름;
 
-use 데이터베이스이름;
+use playdata;
 
 CREATE TABLE account(
 	account_id varchar(20) PRIMARY KEY,
@@ -44,19 +44,24 @@ CREATE TABLE preference(
 );
 
 CREATE TABLE meeting(
-	meeting_id int AUTO_INCREMENT PRIMARY KEY,
-	meeting_name varchar(20) NOT NULL,
-	meeting_date date NOT NULL,
+	meeting_id bigint AUTO_INCREMENT PRIMARY KEY,
+	category varchar(255) NOT NULL,
+    filename varchar(255) NOT NULL,
+    filepath varchar(255) NOT NULL,
+	max_participant int NOT NULL,
 	meeting_detail varchar(50) NOT NULL,
+	master_id varchar(20),
+	meeting_name varchar(20) NOT NULL,
 	meeting_place varchar(20) NOT NULL,
-	master_id varchar(20) NOT NULL,
-	category varchar(20) NOT NULL,
-	max_participant int NOT NULL
+	meeting_place_lat varchar(255) NOT NULL,
+	meeting_place_lng varchar(255) NOT NULL,
+	meeting_start_date date NOT NULL,
+	meeting_end_date date NOT NULL
 );
 
 CREATE TABLE meeting_participant(
 	meeting_participant_id int AUTO_INCREMENT PRIMARY KEY,
-	meeting_id int NOT NULL,
+	meeting_id bigint NOT NULL,
 	participant_id varchar(20) NOT NULL
 );
 	
@@ -64,8 +69,8 @@ ALTER TABLE place ADD FOREIGN KEY (account_id) REFERENCES account  (account_id) 
 ALTER TABLE friend_list ADD FOREIGN KEY (id1) REFERENCES account  (account_id) on delete cascade;
 ALTER TABLE friend_list ADD FOREIGN KEY (id2) REFERENCES account  (account_id) on delete cascade;
 alter table friend_list add constraint ck_friendList check (id1 != id2);
-ALTER TABLE friend_reqeust ADD FOREIGN KEY (request_id) REFERENCES account  (account_id) on delete cascade;
-ALTER TABLE friend_reqeust ADD FOREIGN KEY (requested_id) REFERENCES account  (account_id) on delete cascade;
+ALTER TABLE friend_request ADD FOREIGN KEY (request_id) REFERENCES account  (account_id) on delete cascade;
+ALTER TABLE friend_request ADD FOREIGN KEY (requested_id) REFERENCES account  (account_id) on delete cascade;
 ALTER TABLE preference ADD FOREIGN KEY (account_id) REFERENCES account  (account_id) on delete cascade;
 ALTER TABLE meeting ADD FOREIGN KEY (master_id) REFERENCES account  (account_id) on delete cascade;
 ALTER TABLE meeting_participant ADD FOREIGN KEY (participant_id) REFERENCES account  (account_id) on delete cascade;
@@ -74,7 +79,7 @@ ALTER TABLE meeting_participant ADD FOREIGN KEY (meeting_id) REFERENCES meeting 
 drop table if exists meeting_participant; 
 drop table if exists meeting; 
 drop table if exists preference; 
-drop table if exists friend_reqeust; 
+drop table if exists friend_request; 
 drop table if exists friend_list; 
 drop table if exists hot_place; 
 drop table if exists place; 
